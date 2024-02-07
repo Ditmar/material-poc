f9bd215a2ae1247295fdf77213b199608aa22329
AlexDanielGutierrezCardenas <gutierrezcardenasalexdaniel@gmail.com>
2024-02-07 18:34:48 -0400

    feature/sem-10529221 add files the example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Theme from "./Theme/Theme";
 import NavMenu from "./ui/components/navMenu/NavMenu";
 import { QueryClient, QueryClientProvider } from "react-query";
+import PaginationMenu from "./ui/components/paginationNav/paginationMenu";
 function App() {
   const queryClient = new QueryClient();
   return (
     <Theme>
       <QueryClientProvider client={queryClient}>
-        <NavMenu />
+        {/*<NavMenu />*/}
+        <PaginationMenu/>
       </QueryClientProvider>
     </Theme>
   );
