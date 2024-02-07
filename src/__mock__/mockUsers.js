9db1f2234162e5ed1251a998a0bfd8ce27736b82
daniel <danieldv283.com>
2024-02-07 18:34:56 -0400

    feat(common): added the solution of test

---
+++ src/__mock__/mockUsers.js
@@ -0,0 +1,52 @@
+export const mockedData = {
+    "list" : [
+        {
+            "_id": "1",
+            "username": "Username 1"
+        },
+        {
+            "_id": "2",
+            "username": "Username 2"
+        },
+        {
+            "_id": "3",
+            "username": "Username 3"
+        },
+        {
+            "_id": "1",
+            "username": "Username 1"
+        },
+        {
+            "_id": "2",
+            "username": "Username 2"
+        },
+        {
+            "_id": "3",
+            "username": "Username 3"
+        },
+        {
+            "_id": "1",
+            "username": "Username 1"
+        },
+        {
+            "_id": "2",
+            "username": "Username 2"
+        },
+        {
+            "_id": "3",
+            "username": "Username 3"
+        },
+        {
+            "_id": "1",
+            "username": "Username 1"
+        },
+        {
+            "_id": "2",
+            "username": "Username 2"
+        },
+        {
+            "_id": "3",
+            "username": "Username 3"
+        },
+    ]
+}
--- src/ui/components/navMenu/NavMenu.jsx
+++ src/ui/components/navMenu/NavMenu.jsx
@@ -2,13 +2,15 @@ import { useQuery } from "react-query";
 import { getUsers } from "../../../async/services/userServices";
 import getEnvVariables from "../../../config/configEnvs";
 import { List } from "./list/List";
+import { mockedData } from "../../../__mock__/mockUsers"
 const NavMenu = () => {
   const { HOST, SERVICE } = getEnvVariables();
   const endpoint = `${HOST}${SERVICE}/users`;
   const { data, isLoading, isError } = useQuery("getUsers", () =>
     getUsers(endpoint)
   );
-  return <div>{isLoading ? <div>Loading...</div> : <List data={data} />}</div>;
+  console.log(data);
+  return <div>{isLoading ? <div>Loading...</div> : <List data={mockedData} />}</div>;
 };
 
 export default NavMenu;
